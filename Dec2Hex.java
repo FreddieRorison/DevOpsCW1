f12290475e9c45c27a9033c859dcd1aaaaade8dd
Freddie <freddieror@gmail.com>
2023-11-11 21:33:50 +0000

    Smell Changes

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 class Dec2Hex
 {
 
     public static int Arg1;
-
-    public Dec2Hex() {}
 
     public static void main(String[] args) {
         if (args.length > 0) {
@@ -15,23 +11,26 @@ class Dec2Hex
         }
     }
 
-    public static void DecConvert(String Dec)
+    public static void DecConvert(String dec)
     {
-        if (!isNumber(Dec)) {
+        if (!isNumber(dec)) {
            System.out.println("Input is not a number");
            return;
         }
-        int Arg1 = Integer.parseInt(Dec);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int arg1 = Integer.parseInt(dec);
+        char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
-        num = Arg1;
+        num = arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
         {
             rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
+            StringBuilder stb = new StringBuilder();
+            stb.append(ch[rem]);
+            stb.append(hexadecimal);
+            hexadecimal = stb.toString();
             num= num/16;
         }
 
