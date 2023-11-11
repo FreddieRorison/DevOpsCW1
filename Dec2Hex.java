5c6cc8fd483155d8980da1807e86683af53bceba
Freddie <freddieror@gmail.com>
2023-11-11 21:12:26 +0000

    e

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,7 +8,7 @@ class Dec2Hex
     public Dec2Hex() {}
 
     public static void main(String[] args) {
-        if (args[0]) {
+        if (args[0] != void) {
         DecConvert(args[0]);
         } else {
          System.out.println("No Input Provided");
